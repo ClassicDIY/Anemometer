f4de5bca8963c4ff9ad965025e7386a3507020f0
Graham <graham.a.ross@gmail.com>
2020-04-18 08:20:09 -0400

    Moved constants to platform.ini

--- code/ESP32/src/Anemometer.cpp
+++ code/ESP32/src/Anemometer.cpp
@@ -1,34 +1,42 @@
 #include "Anemometer.h"
 
-
-Anemometer::Anemometer(int sensorPin, float voltageConversionConstant)
+namespace AnemometerNS
 {
-	_sensorPin = sensorPin;
-	_voltageConversionConstant = voltageConversionConstant;
-}
-
-Anemometer::~Anemometer()
-{
-}
-
-float Anemometer::WindSpeed()
-{
-	int sensorValue = analogRead(_sensorPin); //Get a value between 0 and 1023 from the analog pin connected to the anemometer
-
-	float sensorVoltage = sensorValue * _voltageConversionConstant; //Convert sensor value to actual voltage
-
-																   //Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
-	if (sensorVoltage <= voltageMin) {
-		return 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
+	Anemometer::Anemometer(int sensorPin)
+	{
+		_sensorPin = sensorPin;
 	}
-	else {
-		return (sensorVoltage - voltageMin)*windSpeedMax / (voltageMax - voltageMin); //For voltages above minimum value, use the linear relationship to calculate wind speed.
+
+	Anemometer::~Anemometer()
+	{
+	}
+
+	// Wind speed in meters per second
+	float Anemometer::WindSpeed()
+	{
+		// Get a value between 0 and 1023 from the analog pin connected to the anemometer
+		double reading = analogRead(_sensorPin);
+		if (reading < 1 || reading > ADC_Resolution)
+		{
+			return 0;
+		}
+		// The constants used in this calculation are taken from
+		// https://github.com/G6EJD/ESP32-ADC-Accuracy-Improvement-function
+		// and improves the default ADC reading accuracy to within 1%.
+		double sensorVoltage = -0.000000000000016 * pow(reading, 4) +
+			0.000000000118171 * pow(reading, 3) - 0.000000301211691 * pow(reading, 2) +
+			0.001109019271794 * reading + 0.034143524634089;
+
+		// Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
+		if (sensorVoltage <= AnemometerVoltageMin)
+		{
+			// Check if voltage is below minimum value. If so, set wind speed to zero.
+			return 0;
+		}
+		else
+		{
+			// For voltages above minimum value, use the linear relationship to calculate wind speed.
+			return (sensorVoltage - AnemometerVoltageMin) * AnemometerWindSpeedMax / (AnemometerVoltageMax - AnemometerVoltageMin);
+		}
 	}
 }
-
-float Anemometer::SensorVoltage()
-{
-	int sensorValue = analogRead(_sensorPin); //Get a value between 0 and 1023 from the analog pin connected to the anemometer
-	return sensorValue * _voltageConversionConstant; //Convert sensor value to actual voltage
-}
-
--- code/ESP32/src/main.cpp
+++ code/ESP32/src/main.cpp
@@ -11,37 +11,27 @@
 #include <WiFiUdp.h>
 #include <ThreadController.h>
 #include <Thread.h>
-
-// #include <Hash.h>
 #include "Log.h"
 #include "Anemometer.h"
 
-// Don't forget to click vMicro->'Publish Server Data Files' to upload the index.html file for the initial setup of the NodeMCU
+using namespace AnemometerNS;
 
-
-IPAddress apIP(192, 168, 100, 4);
-
-Anemometer _anemometer(ANEMOMETER_ADC_PIN, .0029343066);
-AsyncWebServer  server(80);
-AsyncWebSocket ws("/ws");
-
-const byte        DNS_PORT = 53;
-DNSServer         dnsServer;
+Anemometer _anemometer(AnemometerPin);
+AsyncWebServer _server(80);
+AsyncWebSocket _ws("/ws");
+DNSServer _dnsServer;
 WiFiUDP _udp; // A UDP instance to let us send and receive packets over UDP
-
-const char *myHostname = "anemometer";  //hostname for mDNS. http ://anemometer.local
-
 ThreadController _controller = ThreadController();
-Thread* _workerThreadUdpSend = new Thread();
-Thread* _workerThreadUdpReceive = new Thread();
-Thread* _workerThreadWindMonitor = new Thread();
-Thread* _workerThreadWebServer = new Thread();
+Thread *_workerThreadUdpSend = new Thread();
+Thread *_workerThreadUdpReceive = new Thread();
+Thread *_workerThreadWindMonitor = new Thread();
+Thread *_workerThreadWebServer = new Thread();
 
 unsigned int localPort = 2390; // local port to listen for UDP packets
-IPAddress timeServerIP; 
-const char* ntpServerName = "time.nist.gov"; // time.nist.gov NTP server address
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+IPAddress timeServerIP;
+const char *ntpServerName = "time.nist.gov"; // time.nist.gov NTP server address
+const int NTP_PACKET_SIZE = 48;				 // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[NTP_PACKET_SIZE];			 //buffer to hold incoming and outgoing packets
 
 unsigned long _epoch = 0; //Unix time in seconds
 unsigned long _lastNTP = 0;
@@ -54,18 +44,18 @@ unsigned long _lastHighWindTime;
 float _highWindSpeed = 0;
 
 // send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
+unsigned long sendNTPpacket(IPAddress &address)
 {
-	Serial.println("sending NTP packet...");
+	logd("sending NTP packet...");
 	// set all bytes in the buffer to 0
 	memset(packetBuffer, 0, NTP_PACKET_SIZE);
 	// Initialize values needed to form NTP request
 	// (see URL above for details on the packets)
-	packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-	packetBuffer[1] = 0;     // Stratum, or type of clock
-	packetBuffer[2] = 6;     // Polling Interval
-	packetBuffer[3] = 0xEC;  // Peer Clock Precision
-							 // 8 bytes of zero for Root Delay & Root Dispersion
+	packetBuffer[0] = 0b11100011; // LI, Version, Mode
+	packetBuffer[1] = 0;		  // Stratum, or type of clock
+	packetBuffer[2] = 6;		  // Polling Interval
+	packetBuffer[3] = 0xEC;		  // Peer Clock Precision
+								  // 8 bytes of zero for Root Delay & Root Dispersion
 	packetBuffer[12] = 49;
 	packetBuffer[13] = 0x4E;
 	packetBuffer[14] = 49;
@@ -80,47 +70,51 @@ unsigned long sendNTPpacket(IPAddress& address)
 
 void PrintTime()
 {
-  unsigned long currentTime = _epoch + (millis() - _lastNTP) / 1000;
-  // print the hour, minute and second:
-  Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-  Serial.print((_epoch % 86400L) / 3600); // print the hour (86400 equals secs per day)
-  Serial.print(':');
-  if (((_epoch % 3600) / 60) < 10) {
-    // In the first 10 minutes of each hour, we'll want a leading '0'
-    Serial.print('0');
-  }
-  Serial.print((_epoch % 3600) / 60); // print the minute (3600 equals secs per minute)
-  Serial.print(':');
-  if ((_epoch % 60) < 10) {
-    // In the first 10 seconds of each minute, we'll want a leading '0'
-    Serial.print('0');
-  }
-  Serial.println(_epoch % 60); // print the second
+	unsigned long currentTime = _epoch + (millis() - _lastNTP) / 1000;
+	// print the hour, minute and second:
+	logd("The UTC time is ");		// UTC is the time at Greenwich Meridian (GMT)
+	logd((_epoch % 86400L) / 3600); // print the hour (86400 equals secs per day)
+	logd(':');
+	if (((_epoch % 3600) / 60) < 10)
+	{
+		// In the first 10 minutes of each hour, we'll want a leading '0'
+		logd('0');
+	}
+	logd((_epoch % 3600) / 60); // print the minute (3600 equals secs per minute)
+	logd(':');
+	if ((_epoch % 60) < 10)
+	{
+		// In the first 10 seconds of each minute, we'll want a leading '0'
+		logd('0');
+	}
+	logd(_epoch % 60); // print the second
 
-                //Update for local zone
-  currentTime = currentTime + (_hours_Offset_From_GMT * 60 * 60);
-  Serial.print("The current local time is ");
-  Serial.print((currentTime % 86400L) / 3600); // print the hour (86400 equals secs per day)
-  Serial.print(':');
-  if (((currentTime % 3600) / 60) < 10) {
-    // In the first 10 minutes of each hour, we'll want a leading '0'
-    Serial.print('0');
-  }
-  Serial.print((currentTime % 3600) / 60); // print the minute (3600 equals secs per minute)
-  Serial.print(':');
-  if ((currentTime % 60) < 10) {
-    // In the first 10 seconds of each minute, we'll want a leading '0'
-    Serial.print('0');
-  }
-  Serial.println(currentTime % 60); // print the second
+	//Update for local zone
+	currentTime = currentTime + (_hours_Offset_From_GMT * 60 * 60);
+	logd("The current local time is ");
+	logd((currentTime % 86400L) / 3600); // print the hour (86400 equals secs per day)
+	logd(':');
+	if (((currentTime % 3600) / 60) < 10)
+	{
+		// In the first 10 minutes of each hour, we'll want a leading '0'
+		logd('0');
+	}
+	logd((currentTime % 3600) / 60); // print the minute (3600 equals secs per minute)
+	logd(':');
+	if ((currentTime % 60) < 10)
+	{
+		// In the first 10 seconds of each minute, we'll want a leading '0'
+		logd('0');
+	}
+	logd(currentTime % 60); // print the second
 
-  hour = (currentTime % 86400L) / 3600;
-  if (hour > 12)
-  {
-    hour -= 12;
-  }
-  minute = (currentTime % 3600) / 60;
-  second = currentTime % 60;
+	hour = (currentTime % 86400L) / 3600;
+	if (hour > 12)
+	{
+		hour -= 12;
+	}
+	minute = (currentTime % 3600) / 60;
+	second = currentTime % 60;
 }
 
 void runWindMonitor()
@@ -128,32 +122,34 @@ void runWindMonitor()
 	//PrintTime();
 
 	float windSpeed = _anemometer.WindSpeed();
-	if (_highWindSpeed < windSpeed) {
+	if (_highWindSpeed < windSpeed)
+	{
 		_highWindSpeed = windSpeed;
 		_lastHighWindTime = _epoch + (millis() - _lastNTP) / 1000;
 	}
-	//Serial.print("Wind speed: ");
-	//Serial.print(windSpeed * 3.6);
-	//Serial.println(" Kmh");
+	//logd("Wind speed: ");
+	//logd(windSpeed * 3.6);
+	//logd(" Kmh");
 
-	//Serial.print("SensorVoltage: ");
-	//Serial.print(_anemometer.SensorVoltage(), 6);
-	//Serial.println(" ");
-	if (ws.enabled()) {
+	//logd("SensorVoltage: ");
+	//logd(_anemometer.SensorVoltage(), 6);
+	//logd(" ");
+	if (_ws.enabled())
+	{
 		StaticJsonDocument<128> root;
 		String s;
 		root["ws"] = windSpeed * 3.6;
 		root["hws"] = _highWindSpeed * 3.6;
 		root["hwt"] = _lastHighWindTime;
 		serializeJson(root, s);
-		ws.textAll(s.c_str());
-		// Serial.println(s);
+		_ws.textAll(s.c_str());
+		logd(s);
 	}
 }
 
 // void runWebServer()
 // {
-// 	dnsServer.processNextRequest();
+// 	_dnsServer.processNextRequest();
 // 	// server.handleClient();
 // 	// webSocket.loop();
 // }
@@ -168,9 +164,10 @@ void runUdpSend()
 void runUdpReceive()
 {
 	int cb = _udp.parsePacket();
-	if (cb) {
-		Serial.print("packet received, length=");
-		Serial.println(cb);
+	if (cb)
+	{
+		logd("packet received, length=");
+		logd(cb);
 		// We've received a packet, read the data from it
 		_udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
 		//the timestamp starts at byte 40 of the received packet and is four bytes,  or two words, long. First, esxtract the two words:
@@ -179,10 +176,10 @@ void runUdpReceive()
 		// combine the four bytes (two words) into a long integer
 		// this is NTP time (seconds since Jan 1 1900):
 		unsigned long secsSince1900 = highWord << 16 | lowWord;
-		Serial.print("Seconds since Jan 1 1900 = ");
-		Serial.println(secsSince1900);
+		logd("Seconds since Jan 1 1900 = ");
+		logd(secsSince1900);
 		// now convert NTP time into everyday time:
-		Serial.print("Unix time = ");
+		logd("Unix time = ");
 		// Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
 		const unsigned long seventyYears = 2208988800UL;
 		// subtract seventy years:
@@ -191,122 +188,148 @@ void runUdpReceive()
 	}
 }
 
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
-	Serial.printf("onWsEvent");
-  if(type == WS_EVT_CONNECT){
-    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
-    client->printf("Hello Client %u :)", client->id());
-    client->ping();
-  } else if(type == WS_EVT_DISCONNECT){
-    Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
-  } else if(type == WS_EVT_ERROR){
-    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
-  } else if(type == WS_EVT_PONG){
-    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
-  } else if(type == WS_EVT_DATA){
-    AwsFrameInfo * info = (AwsFrameInfo*)arg;
-    String msg = "";
-    if(info->final && info->index == 0 && info->len == len){
-      //the whole message is in a single frame and we got all of it's data
-      Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+	logd("onWsEvent");
+	if (type == WS_EVT_CONNECT)
+	{
+		logd("ws[%s][%u] connect\n", server->url(), client->id());
+		client->printf("Hello Client %u :)", client->id());
+		client->ping();
+	}
+	else if (type == WS_EVT_DISCONNECT)
+	{
+		logd("ws[%s][%u] disconnect\n", server->url(), client->id());
+	}
+	else if (type == WS_EVT_ERROR)
+	{
+		logd("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
+	}
+	else if (type == WS_EVT_PONG)
+	{
+		logd("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
+	}
+	else if (type == WS_EVT_DATA)
+	{
+		AwsFrameInfo *info = (AwsFrameInfo *)arg;
+		String msg = "";
+		if (info->final && info->index == 0 && info->len == len)
+		{
+			//the whole message is in a single frame and we got all of it's data
+			logd("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
+			if (info->opcode == WS_TEXT)
+			{
+				for (size_t i = 0; i < info->len; i++)
+				{
+					msg += (char)data[i];
+				}
+			}
+			else
+			{
+				char buff[3];
+				for (size_t i = 0; i < info->len; i++)
+				{
+					sprintf(buff, "%02x ", (uint8_t)data[i]);
+					msg += buff;
+				}
+			}
+			logd("%s\n", msg.c_str());
 
-      if(info->opcode == WS_TEXT){
-        for(size_t i=0; i < info->len; i++) {
-          msg += (char) data[i];
-        }
-      } else {
-        char buff[3];
-        for(size_t i=0; i < info->len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
-        }
-      }
-      Serial.printf("%s\n",msg.c_str());
+			if (info->opcode == WS_TEXT)
+				client->text("I got your text message");
+			else
+				client->binary("I got your binary message");
+		}
+		else
+		{
+			//message is comprised of multiple frames or the frame is split into multiple packets
+			if (info->index == 0)
+			{
+				if (info->num == 0)
+					logd("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+				logd("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+			}
 
-      if(info->opcode == WS_TEXT)
-        client->text("I got your text message");
-      else
-        client->binary("I got your binary message");
-    } else {
-      //message is comprised of multiple frames or the frame is split into multiple packets
-      if(info->index == 0){
-        if(info->num == 0)
-          Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
-        Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
-      }
+			logd("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
 
-      Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
-
-      if(info->opcode == WS_TEXT){
-        for(size_t i=0; i < len; i++) {
-          msg += (char) data[i];
-        }
-      } else {
-        char buff[3];
-        for(size_t i=0; i < len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
-        }
-      }
-      Serial.printf("%s\n",msg.c_str());
-
-      if((info->index + len) == info->len){
-        Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
-        if(info->final){
-          Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
-          if(info->message_opcode == WS_TEXT)
-            client->text("I got your text message");
-          else
-            client->binary("I got your binary message");
-        }
-      }
-    }
-  }
+			if (info->opcode == WS_TEXT)
+			{
+				for (size_t i = 0; i < len; i++)
+				{
+					msg += (char)data[i];
+				}
+			}
+			else
+			{
+				char buff[3];
+				for (size_t i = 0; i < len; i++)
+				{
+					sprintf(buff, "%02x ", (uint8_t)data[i]);
+					msg += buff;
+				}
+			}
+			logd("%s\n", msg.c_str());
+			if ((info->index + len) == info->len)
+			{
+				logd("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+				if (info->final)
+				{
+					logd("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+					if (info->message_opcode == WS_TEXT)
+						client->text("I got your text message");
+					else
+						client->binary("I got your binary message");
+				}
+			}
+		}
+	}
 }
 
-void notFound(AsyncWebServerRequest *request) {
-    request->send(404, "text/plain", "Not found");
+void notFound(AsyncWebServerRequest *request)
+{
+	request->send(404, "text/plain", "Not found");
 }
 
-const char* ssid = "SkyeNet";
-const char* password = "acura22546";
-const char * hostName = "esp-async";
-const char* http_username = "admin";
-const char* http_password = "admin";
+const char *ssid = "SkyeNet";
+const char *password = "acura22546";
+const char *hostName = "esp-async";
+const char *http_username = "admin";
+const char *http_password = "admin";
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("Port Openned");
+	Serial.begin(115200);
+	logd();
+	logd("Port Openned");
 
-//   WiFiManager wifiManager;
-//   //wifiManager.resetSettings(); //reset settings - for testing
-//   wifiManager.setTimeout(180);
-//   if (!wifiManager.autoConnect("AutoConnectAP")) {
-//     Serial.println("failed to connect and hit timeout");
-//     delay(3000);
-//     //reset and try again, or maybe put it to deep sleep
-//     ESP.reset();
-//     delay(5000);
-//   }
+	//   WiFiManager wifiManager;
+	//   //wifiManager.resetSettings(); //reset settings - for testing
+	//   wifiManager.setTimeout(180);
+	//   if (!wifiManager.autoConnect("AutoConnectAP")) {
+	//     logd("failed to connect and hit timeout");
+	//     delay(3000);
+	//     //reset and try again, or maybe put it to deep sleep
+	//     ESP.reset();
+	//     delay(5000);
+	//   }
 	WiFi.softAP(hostName);
 	WiFi.begin(ssid, password);
-	if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-		Serial.printf("STA: Failed!\n");
+	if (WiFi.waitForConnectResult() != WL_CONNECTED)
+	{
+		logd("STA: Failed!\n");
 		WiFi.disconnect(false);
 		delay(1000);
 		WiFi.begin(ssid, password);
 	}
 
-	Serial.println("WiFi connected");
-	Serial.print("IP address: ");
-	Serial.println(WiFi.localIP());
+	logd("WiFi connected");
+	logd("IP address: ");
+	logd(WiFi.localIP());
 
-	Serial.println("Starting UDP");
+	logd("Starting UDP");
 	_udp.begin(localPort);
-	Serial.print("UDP IP: ");
-	Serial.println(_udp.remoteIP());
+	logd("UDP IP: ");
+	logd(_udp.remoteIP());
 	// Configure main worker thread
 	// _workerThreadUdpSend->onRun(runUdpSend);
 	// _workerThreadUdpSend->setInterval(600000);
@@ -322,36 +345,38 @@ void setup()
 	// _workerThreadWebServer->setInterval(200);
 	// _controller.add(_workerThreadWebServer);
 
-	MDNS.addService("http","tcp",80);
-	if(!SPIFFS.begin()){
-		Serial.println("Failed to mount file system");
+	MDNS.addService("http", "tcp", 80);
+	if (!SPIFFS.begin())
+	{
+		logd("Failed to mount file system");
 	}
-	else {
-		Serial.println("File system mounted");
+	else
+	{
+		logd("File system mounted");
 		File root = SPIFFS.open("/");
 		File file = root.openNextFile();
-		while(file){
-		
-			Serial.print("FILE: ");
-			Serial.println(file.name());
+		while (file)
+		{
+
+			logd("FILE: ");
+			logd(file.name());
 			file = root.openNextFile();
 		}
-		server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.htm");
+		_server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.htm");
 	}
-	server.on("/heap", HTTP_GET, [](AsyncWebServerRequest *request){
+	_server.on("/heap", HTTP_GET, [](AsyncWebServerRequest *request) {
 		request->send(200, "text/plain", String(ESP.getFreeHeap()));
 	});
 
-	ws.onEvent(onWsEvent);
-	server.addHandler(&ws);
-	server.onNotFound(notFound);
-	server.begin();
-	Serial.println("Setup Done");
-  
+	_ws.onEvent(onWsEvent);
+	_server.addHandler(&_ws);
+	_server.onNotFound(notFound);
+	_server.begin();
+	logd("Setup Done");
 }
 
 void loop()
 {
 	_controller.run();
-	ws.cleanupClients();
+	_ws.cleanupClients();
 }
